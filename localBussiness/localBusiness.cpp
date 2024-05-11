4bdea5639a0ad356a8d3f8cf1f50757683c1ecd0
lining <770677655@qq.com>
2024-05-11 15:24:02 +0800

    修改逻辑错误

--- localBussiness/localBusiness.cpp
+++ localBussiness/localBusiness.cpp
@@ -73,7 +73,7 @@ void LocalBusiness::Task_Keep(void *p) {
 //    LOG(INFO) << __FUNCTION__ << " START";
     auto local = (LocalBusiness *) p;
 
-    if (local->serverList.empty() || local->clientList.empty()) {
+    if (local->serverList.empty() && local->clientList.empty()) {
         return;
     }
 
